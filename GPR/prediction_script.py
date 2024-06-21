a0700b76688dee12ed16634ac5e6024126aa8fc5
Nechev <aleksandar.nechev@tno.nl>
2024-06-21 11:57:48 +0200

    Scripts

---
+++ GPR/prediction_script.py
@@ -0,0 +1,143 @@
+import pickle
+import numpy as np
+import math
+import json
+import pandas as pd
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern, ConstantKernel
+from skopt import gp_minimize
+from skopt.space import Real
+from sklearn.metrics import mean_squared_error
+
+MODEL_PATH = '/app/model.pkl'
+INPUT_JSON_PATH = '/shared_data/vs/input.json'  # Adjust the path as needed
+OUTPUT_JSON_PATH = '/shared_data/vs/output.json' 
+
+def sensors_value_to_matrix(dict_sensors_t):
+    X_rs = []
+    Y_rs = []
+    X_tot = []
+    Y_tot = []
+    Z_rs = []
+    for sensor in dict_sensors_t.keys():
+        if not (np.isnan(dict_sensors_t[sensor]["value"]) or math.isnan(dict_sensors_t[sensor]["value"]) or pd.isna(dict_sensors_t[sensor]["value"])):
+            X_rs.append(dict_sensors_t[sensor]['x'])
+            Y_rs.append(dict_sensors_t[sensor]['y'])
+            Z_rs.append(dict_sensors_t[sensor]['value'])
+        X_tot.append(dict_sensors_t[sensor]['x'])
+        Y_tot.append(dict_sensors_t[sensor]['y'])
+    X_rs = np.array(X_rs)
+    X_tot = np.array(X_tot)
+    Y_rs = np.array(Y_rs)
+    Y_tot = np.array(Y_tot)
+    Z_rs = np.array(Z_rs)
+    
+    y_mean, y_std = np.mean(Y_rs), np.std(Y_rs)
+    Y_tot = (Y_tot - y_mean) / y_std
+    Y_rs = (Y_rs - y_mean) / y_std
+
+    x_mean, x_std = np.mean(X_rs), np.std(X_rs)
+    X_tot = (X_tot - x_mean) / x_std
+    X_rs = (X_rs - x_mean) / x_std
+
+    z_mean = np.mean(Z_rs)
+    z_std = np.std(Z_rs)
+    Z_rs = (Z_rs - z_mean) / z_std
+
+    return {'x': X_rs, 'y': Y_rs, 'x_tot': X_tot, 'y_tot': Y_tot, 'z': Z_rs, 'z_mean': z_mean, 'z_std': z_std, 'x_mean': x_mean, 'x_std': x_std, 'y_mean': y_mean, 'y_std': y_std}
+
+def all_training_values(dict_sensors):
+    all_train = []
+    for timestamp in dict_sensors.keys():
+        dict_matrix_values = sensors_value_to_matrix(dict_sensors[timestamp])
+        A_train = np.column_stack((dict_matrix_values['x'], dict_matrix_values['y']))
+        Z_train = dict_matrix_values['z']
+        all_train.append((A_train, Z_train))
+    return all_train
+
+def hyperparameters_training(dict_sensors, bounds_constant_value=np.logspace(-1, 1, 10), bounds_length_scale=np.logspace(-1, 1, 10), kernel=None, model=None):
+    all_train = all_training_values(dict_sensors)
+    if kernel is None:
+        kernel = matern_kernel(bounds_constant_value[0], bounds_length_scale[0], 1/2)
+    if model is None:
+        model = GaussianProcessRegressor(kernel=kernel)
+
+    def objective(params):
+        k1, k2 = params
+        mse_total = 0
+        for (A_train, Z_train) in all_train:
+            model.kernel.k1.constant_value = k1
+            model.kernel.k2.length_scale = k2
+            model.fit(A_train, Z_train)
+            Z_pred = model.predict(A_train)
+            mse_total += mean_squared_error(Z_train, Z_pred)
+        return mse_total / len(all_train)
+
+    param_space = [Real(bounds_constant_value[0], bounds_constant_value[-1], name='k1'),
+                   Real(bounds_length_scale[0], bounds_length_scale[-1], name='k2')]
+    result = gp_minimize(objective, param_space, n_calls=20, random_state=42)
+    best_k1, best_k2 = result.x
+    kernel = matern_kernel(best_k1, best_k2, 1/2)
+    model = GaussianProcessRegressor(kernel=kernel)
+    return model
+
+def matern_kernel(constant_value, length_scale, nu):
+    return ConstantKernel(constant_value=constant_value) * Matern(length_scale=length_scale, nu=nu)
+
+class GPRModel:
+    def __init__(self, constant_value=1, length_scale=1, nu=3/2):
+        self.constant_value = constant_value
+        self.length_scale = length_scale
+        self.kernel = ConstantKernel(constant_value=constant_value) * Matern(length_scale=length_scale, nu=nu)
+        self.model = GaussianProcessRegressor(kernel=self.kernel)
+
+    def training(self, dict_sensors):
+        self.model = hyperparameters_training(dict_sensors, bounds_constant_value=[1e-1, 1e1], bounds_length_scale=[1e-1, 1e1], kernel=self.kernel, model=self.model)
+        self.kernel = self.model.kernel
+        self.constant_value = self.kernel.k1.constant_value
+        self.length_scale = self.kernel.k2.length_scale
+    
+    def predict(self, snapshot):
+        mod = self.model
+        dict_matrix_val = sensors_value_to_matrix(snapshot)
+        A_fit = np.column_stack((dict_matrix_val['x'], dict_matrix_val['y']))
+        Z_fit = dict_matrix_val['z']
+        A = np.column_stack((dict_matrix_val['x_tot'], dict_matrix_val['y_tot']))
+        mod = mod.fit(A_fit, Z_fit)
+        return A, mod.predict(A)
+
+    def reset_parameters(self):
+        self.constant_value = 1
+        self.length_scale = 1
+        self.kernel = ConstantKernel(constant_value=self.constant_value) * Matern(length_scale=self.length_scale, nu=3/2)
+        self.model = GaussianProcessRegressor(kernel=self.kernel)
+
+    def save_model(self, save_dir):
+        with open(save_dir, 'wb') as file:
+            pickle.dump(self, file)
+
+if __name__ == "__main__":
+        
+    with open(MODEL_PATH, 'rb') as file:
+        model = pickle.load(file)
+
+    if model is None:
+        print("Unable to load the model.")
+        exit(1)
+
+    with open(INPUT_JSON_PATH, 'r') as input_file:
+        input_data = json.load(input_file)
+
+    if input_data is None:
+        print("No valid input data found.")
+        exit(1)
+
+    print(input_data)
+    print(model)
+    
+    predictions = model.predict(input_data)
+    predictions = {'coordinates': predictions[0].tolist(), 'predictions': predictions[1].tolist()}
+
+    with open(OUTPUT_JSON_PATH, 'w') as f:
+        json.dump(predictions, f)
+
---
+++ Test_model/test_prediction_script.py
@@ -0,0 +1,47 @@
+import json
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def load_model(model_path):
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+def load_input_data(input_json_path):
+    with open(input_json_path, 'r') as file:
+        input_data = json.load(file)
+    return np.array(input_data['features'])
+
+def save_predictions(predictions, output_json_path):
+    with open(output_json_path, 'w') as file:
+        json.dump({'predictions': predictions.tolist()}, file)
+
+def infer(model_path, input_json_path, output_json_path):
+    # Load the model
+    model = load_model(model_path)
+    
+    # Load the input data
+    features = load_input_data(input_json_path)
+    
+    # Apply the same scaling
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)  # Assuming same scaling as training
+    
+    # Make predictions
+    predictions = model.predict(features_scaled)
+    
+    # Save the predictions
+    save_predictions(predictions, output_json_path)
+
+if __name__ == "__main__":
+    # Define file paths
+    MODEL_PATH = '/app/model.pkl'  # Path to the saved model
+    INPUT_JSON_PATH = '/shared_data/vs/input.json'  # Path to the input JSON file
+    OUTPUT_JSON_PATH = '/shared_data/vs/output.json'  # Path to the output JSON file
+    # MODEL_PATH = 'C:/Users/nechevah/DTIC Project/Implementation/VS-DTIC MVP/script_dummy/model.pkl'  # Path to the saved model
+    # INPUT_JSON_PATH = 'C:/Users/nechevah/DTIC Project/Implementation/VS-DTIC MVP/script_dummy/inherence.json'  # Path to the input JSON file
+    # OUTPUT_JSON_PATH = 'output.json'  # Path to the output JSON file
+
+    # Run inference and save predictions
+    infer(MODEL_PATH, INPUT_JSON_PATH, OUTPUT_JSON_PATH)
